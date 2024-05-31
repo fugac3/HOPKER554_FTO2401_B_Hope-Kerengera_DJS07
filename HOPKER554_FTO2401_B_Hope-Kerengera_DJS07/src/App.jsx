3dd2dcc9c4420aa1460efc4c058b70d2c939cbf1
Hope <k.hope.ing@gmail.com>
2024-05-31 11:43:15 +0200

    Created Header Component and Added Basic Styling

--- HOPKER554_FTO2401_B_Hope-Kerengera_DJS07/src/App.jsx
+++ HOPKER554_FTO2401_B_Hope-Kerengera_DJS07/src/App.jsx
@@ -1,11 +1,10 @@
+import React from 'react'
+import Header from './components/Header.jsx'
 
-function App() {
-
+export default function App() {
   return (
-    <>
-     
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
-
-export default App
---
+++ HOPKER554_FTO2401_B_Hope-Kerengera_DJS07/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img
+                src="./images/meme-face-logo.png"
+                className="header--image"
+                alt="Meme Logo"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project</h4>
+        </header>
+    )
+}
